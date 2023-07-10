bb19b6e7f168fe79062be40bf562041e5f942a7d
Soskretkov <oskrserg@gmail.com>
2023-07-10 12:19:26 +0300

    get_string() не экспортируется, vba парсит структуру

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,41 @@
-use std::os::raw::c_void;
-
 // структура с данными по которым vba считает строку из памяти, #[repr(C)] зафиксирует поля в порядке как задал программист
+//Если есть только Box<Vec<u16>> без Option, не сможете использовать std::mem::forget, поскольку Box<Vec<u16>> всегда должен иметь действительное значение. Option<Box<Vec<u16>>> позволяет использовать None для обозначения отсутствия значения, что позволяет безопасно использовать std::mem::forget.
 #[repr(C)]
 pub struct SendingStringForVBA {
-    ptr: *mut c_void,
-    length: i32,
+    ptr: *mut u16,
+    length_in_bytes: i32,
+    _data: Option<Box<Vec<u16>>>, // это поле будет читаться VBA
 }
 
 #[no_mangle]
-pub extern "stdcall" fn send_request() -> *mut String {
+pub extern "stdcall" fn send_request() -> *mut SendingStringForVBA {
     let text = getDatabaseResponse();
-    let text_ptr = Box::new(text);
+    let sending_data = get_string(text);
 
-    Box::into_raw(text_ptr)
+    Box::into_raw(Box::new(sending_data))
+}
+
+// Преобразует String в SendingStringForVBA
+fn get_string(text: String) -> SendingStringForVBA {
+    //.encode_utf16() правильно обрабатывает суррогатные пары Unicode (возвращает итератор, который выдает 16-битные юниты кодировки UTF-16).
+    //тогда как chars().count() возвращает количество Unicode скаляров в строке, не все из которых будут соответствовать одному символу в строке
+    let data: Vec<u16> = text.encode_utf16().collect();
+    let length_in_bytes: i32 = (text.encode_utf16().count() * std::mem::size_of::<u16>()).try_into().unwrap();
+
+    let boxed_data = Box::new(data);
+    let ptr = boxed_data.as_ptr() as *mut u16;
+
+    SendingStringForVBA {
+        ptr,
+        length_in_bytes,
+        _data: Some(boxed_data),
+    }
 }
 
 #[no_mangle]
-pub extern "stdcall" fn get_string(ptr: *mut String) -> Box<SendingStringForVBA> {
-    let mut data: Vec<u16> = unsafe { (*ptr).encode_utf16().collect() };
-    free_data(ptr);
-    data.push(0); // null terminate
-
-    let sending_data = Box::new(SendingStringForVBA {
-        ptr: data.as_mut_ptr() as *mut c_void,
-        length: (data.len() * std::mem::size_of::<u16>())
-            .try_into()
-            .unwrap(),
-    });
-
-    sending_data
-}
-
-#[no_mangle]
-pub extern "stdcall" fn free_data(ptr: *mut String) {
+pub extern "stdcall" fn free_data(ptr: *mut SendingStringForVBA) {
     unsafe {
-        // Освобождение памяти, на которую указывает ptr
+        // освобождаем память под структурой
         drop(Box::from_raw(ptr));
     }
 }
