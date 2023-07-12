120a53598ca22fdeeda9e39e051b92745ce5fd6e
Soskretkov <oskrserg@gmail.com>
2023-07-12 11:46:02 +0300

    закончил модуль входящей строки от vba + рефакторинг lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 mod vba_string_utils;
-use vba_string_utils as vsu;
+use vba_string_utils::StringForVBA;
 
 #[no_mangle]
-pub extern "stdcall" fn send_request(ptr: *const u16) -> *mut vsu::StringWrapForVBA {
-    let sql_query = vsu::get_string_from_vba(ptr);
+pub extern "stdcall" fn send_request(ptr: *const u16) -> *mut StringForVBA {
+    // let sql_query = vba_string_utils::get_string_from_vba(ptr);
+    let sql_query = String::from("select * from ref_currency_type;");
 
     let response = getDatabaseResponse(sql_query);
-
-    vsu::get_string_ptr_for_vba(response)
+    let response_for_vba = StringForVBA::from_string(response);
+    response_for_vba.into_raw()
 }
 
 #[no_mangle]
-pub extern "stdcall" fn free_data(ptr: *mut vsu::StringWrapForVBA) {
+pub extern "stdcall" fn free_data(ptr: *mut StringForVBA) {
     unsafe {
         // освобождаем память под структурой
         drop(Box::from_raw(ptr));
--- src/vba_string_utils.rs
+++ src/vba_string_utils.rs
@@ -1,13 +1,37 @@
-
 use std::slice;
-// структура с данными по которым vba получит строку из памяти, #[repr(C)] зафиксирует поля в порядке как задал программист
 
+// структура по которой vba получает строку, #[repr(C)] фиксирует поля в порядке как задал программист
 #[repr(C)]
-pub struct StringWrapForVBA {
+pub struct StringForVBA {
     ptr: *mut u16,
     length_in_bytes: i32,
     _data: Box<Vec<u16>>, // это поле не будет читаться VBA и оно тут для выравнивания времени жизни с полем "ptr" для того чтобы vba читал действительный "ptr"
 }
+
+impl StringForVBA {
+    pub fn from_string(text: String) -> Self {
+        //.encode_utf16() правильно обрабатывает суррогатные пары Unicode (возвращает итератор 16-битных юнитов кодировки UTF-16).
+        let data: Vec<u16> = text.encode_utf16().collect();
+        let length_in_bytes: i32 = (text.encode_utf16().count() * std::mem::size_of::<u16>())
+            .try_into()
+            .unwrap();
+
+        let boxed_data = Box::new(data);
+        let ptr = boxed_data.as_ptr() as *mut u16;
+
+        let sending_data = StringForVBA {
+            ptr,
+            length_in_bytes,
+            _data: boxed_data,
+        };
+        sending_data
+    }
+
+    pub fn into_raw(self) -> *mut StringForVBA {
+        Box::into_raw(Box::new(self))
+    }
+}
+
 
 pub fn get_string_from_vba(bstr_ptr: *const u16) -> String {
     // вычисление длины строки
@@ -18,22 +42,4 @@ pub fn get_string_from_vba(bstr_ptr: *const u16) -> String {
     // создание среза: второй параметр это число итераций, длина итерантов определяется по типу указателя (2 байта у нас)
     let slice = unsafe { slice::from_raw_parts(bstr_ptr, (bstr_len_in_bytes / 2) as usize) }; // создание среза
     String::from_utf16(slice).unwrap()
-}
-
-pub fn get_string_ptr_for_vba(text: String) -> *mut StringWrapForVBA {
-    //.encode_utf16() правильно обрабатывает суррогатные пары Unicode (возвращает итератор 16-битных юнитов кодировки UTF-16).
-    let data: Vec<u16> = text.encode_utf16().collect();
-    let length_in_bytes: i32 = (text.encode_utf16().count() * std::mem::size_of::<u16>())
-        .try_into()
-        .unwrap();
-
-    let boxed_data = Box::new(data);
-    let ptr = boxed_data.as_ptr() as *mut u16;
-
-    let sending_data = StringWrapForVBA {
-        ptr,
-        length_in_bytes,
-        _data: boxed_data,
-    };
-    Box::into_raw(Box::new(sending_data))
 }
