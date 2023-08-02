b2772b19ec08d5cff0b78672d7831f7f9d6a40b7
Soskretkov <oskrserg@gmail.com>
2023-08-03 01:06:10 +0300

    несколько запросов принимает

--- excel_dll_postgres_rust/src/lib.rs
+++ excel_dll_postgres_rust/src/lib.rs
@@ -2,54 +2,90 @@ use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
 mod vba_str_io;
+use chrono::NaiveDate;
+use indexmap::IndexMap;
+use serde::ser::{SerializeMap, Serializer};
 use serde::Deserialize;
+use serde::Serialize;
+use serde_json::Value;
 use tokio;
 use tokio_postgres::{types::Type, NoTls, Row};
 use vba_str_io::StringForVBA;
 
-#[derive(Debug, Deserialize)]
-struct VBARequest {
-    sheet_name: String,
+#[derive(Deserialize)]
+struct ExcelRequest {
     sql_query: String,
-    pg_current_wal_lsn: String,
+    requesters_id: Option<String>, //имя листа или таблицы
 }
 
-impl FromStr for VBARequest {
+impl FromStr for ExcelRequest {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(serde_json::from_str(s).map_err(Error::JsonDeserializationError)?)
+        Ok(serde_json::from_str(s).map_err(Error::JsonDeserialization)?)
+    }
+}
+
+#[derive(Serialize)]
+struct ExcelResponse {
+    data: Result<Vec<OrderedJson>, Error>,
+    requesters_id: Option<String>,
+}
+
+// сиротское правило не дает реализовать трейт Serialize непосредственно на IndexMap
+struct OrderedJson(IndexMap<String, Value>);
+
+impl Serialize for OrderedJson {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let OrderedJson(ref inner_map) = *self;
+        let mut map = serializer.serialize_map(Some(inner_map.len()))?;
+        for (k, v) in inner_map {
+            map.serialize_entry(k, v)?;
+        }
+        map.end()
+    }
+}
+
+impl OrderedJson {
+    pub fn new() -> Self {
+        OrderedJson(IndexMap::new())
+    }
+    pub fn insert(&mut self, key: String, value: Value) {
+        self.0.insert(key, value);
     }
 }
 
 #[no_mangle]
 pub extern "stdcall" fn send_request(ptr: *const u16) -> *mut StringForVBA {
-    let wraped_response: Result<String, Error> = {
+    let wraped_sent_json_txt: Result<_, Error> = {
         || {
-            let string_from_vba = vba_str_io::get_string_from_vba(ptr)
-                .map_err(|_| Error::InvalidUTF16FromVbaError)?;
+            let string_from_vba =
+                vba_str_io::get_string_from_vba(ptr).map_err(|_| Error::InvalidUTF16FromVba)?;
 
-            let vba_request = VBARequest::from_str(&string_from_vba)?;
+            let excel_requests: Vec<ExcelRequest> =
+                serde_json::from_str(&string_from_vba).map_err(Error::JsonDeserialization)?;
 
             let my_db_params = get_db_params(); // параметры для подключения к БД
 
-            let db_response = get_database_response(&vba_request.sql_query, my_db_params)?; // ответ БД
+            let rows_vec = get_database_response(&excel_requests, my_db_params)?; // ответ БД
 
-            let json = rows_type_into_obj_in_arr_json(db_response)?;
+            let orderedjson_vec = rows_type_into_obj_in_arr_json(excel_requests, rows_vec);
 
-            Ok(json)
+            let sent_json_txt =
+                serde_json::to_string(&orderedjson_vec).map_err(Error::JsonSerialization);
+
+            Ok(orderedjson_vec)
         }
     }();
 
-    let (is_valid, response) = match wraped_response {
-        Ok(response) => (true, response),
-        Err(err) => (false, format!("{}", err)),
-    };
+    let sent_json_txt = serde_json::to_string(&wraped_sent_json_txt.unwrap()).unwrap();
 
     // конвертация в формат, ожидаемый на стороне vba
-    let mut response_for_vba = StringForVBA::from_string(response);
-    response_for_vba.validity_update(is_valid);
-    response_for_vba.into_raw()
+    let string_for_vba = StringForVBA::from_string(sent_json_txt);
+    string_for_vba.into_raw()
 }
 
 #[no_mangle]
@@ -62,9 +98,10 @@ pub extern "stdcall" fn free_data(ptr: *mut StringForVBA) {
 
 //для вызова из кода на других языках, используется соглашение о вызове stdcall (обычно используемое в Windows для вызовов функций API)
 fn get_database_response(
-    query: &str,
+    excel_requests: &[ExcelRequest],
     db_access_parameters: HashMap<String, String>,
-) -> Result<Vec<Row>, Error> {
+) -> Result<Vec<Result<Vec<Row>, Error>>, Error> {
+    // ) -> Option<Result<Vec<Row>, Error>> {
     // строка параметров для соединения с БД
     let parameter_string = format!(
         "host={} dbname={} user={} password={}",
@@ -75,12 +112,12 @@ fn get_database_response(
     );
 
     // Tokio автоматически создает рантайм для асинхронных операций, но ниже это делается вручную - код не в асинхронной среде
-    let rt = tokio::runtime::Runtime::new().map_err(Error::TokioRuntimeCreationError)?;
+    let rt = tokio::runtime::Runtime::new().map_err(Error::TokioRuntimeCreation)?;
 
     // NoTls - не требуетя защищенного соединения, что приемлемо в защищенной среде
     let (client, connection) = rt
         .block_on(tokio_postgres::connect(&parameter_string, NoTls))
-        .map_err(Error::DBConnectionError)?;
+        .map_err(Error::DBConnection)?;
 
     // запускает асинхронную задачу, которая ожидает завершения соединения с БД. Если ошибка, она будет записана в стандартный поток ошибок
     rt.spawn(async move {
@@ -89,108 +126,102 @@ fn get_database_response(
         }
     });
 
-    rt.block_on(client.query(query, &[]))
-        .map_err(Error::SqlExecutionError)
+    let mut res: Vec<Result<Vec<Row>, Error>> = Vec::new();
+    for request in excel_requests {
+        res.push(
+            rt.block_on(client.query(&request.sql_query, &[]))
+                .map_err(Error::SqlExecution),
+        );
+    }
+
+    Ok(res)
 }
 
-fn rows_type_into_obj_in_arr_json(rows: Vec<Row>) -> Result<String, Error> {
-    use chrono::NaiveDate;
-    use indexmap::IndexMap;
-    use serde::ser::{SerializeMap, Serializer};
-    use serde::Serialize;
-    use serde_json::Value;
+fn rows_type_into_obj_in_arr_json(
+    excel_requests: Vec<ExcelRequest>,
+    data_vec: Vec<Result<Vec<Row>, Error>>,
+) -> Vec<ExcelResponse> {
+    let mut res = Vec::with_capacity(excel_requests.len());
+    for (request, rows_vec) in excel_requests.into_iter().zip(data_vec) {
+        let data = match rows_vec {
+            Ok(rows) => {
+                let inner_result: Vec<_> = rows
+                    .into_iter()
+                    .map(|row| {
+                        let mut hmap: OrderedJson = OrderedJson::new();
 
-    // сиротское правило не дает реализовать трейт Serialize непосредственно на IndexMap
-    struct OrderedJson(IndexMap<String, Value>);
-
-    impl Serialize for OrderedJson {
-        fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            let OrderedJson(ref inner_map) = *self;
-            let mut map = serializer.serialize_map(Some(inner_map.len()))?;
-            for (k, v) in inner_map {
-                map.serialize_entry(k, v)?;
+                        for (i, column) in row.columns().into_iter().enumerate() {
+                            let k = column.name().to_string();
+                            let v: serde_json::Value = match *column.type_() {
+                                Type::BOOL => match row.try_get::<_, bool>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::CHAR => match row.try_get::<_, i8>(i) {
+                                    Ok(v) => {
+                                        let ch = char::from_u32(v as u32).unwrap_or('\0');
+                                        serde_json::json!(ch.to_string())
+                                    }
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::INT2 => match row.try_get::<_, i16>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::INT4 => match row.try_get::<_, i32>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::OID => match row.try_get::<_, u32>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::INT8 => match row.try_get::<_, i64>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::FLOAT4 => match row.try_get::<_, f32>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::FLOAT8 => match row.try_get::<_, f64>(i) {
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                Type::DATE => match row.try_get::<_, NaiveDate>(i) {
+                                    Ok(v) => {
+                                        // let base_date = NaiveDate::from_ymd_opt(1899, 12, 30).unwrap();
+                                        // let duration = v.signed_duration_since(base_date);
+                                        // serde_json::json!(duration.num_days())
+                                        serde_json::json!(v.format("%Y-%m-%d").to_string())
+                                    }
+                                    Err(_) => serde_json::json!(null),
+                                },
+                                _ => match row.try_get::<_, String>(i) {
+                                    //VARCHAR, CHAR(n), TEXT, CITEXT, NAME
+                                    Ok(v) => serde_json::json!(v),
+                                    Err(_) => serde_json::json!(null),
+                                },
+                            };
+                            hmap.insert(k, v);
+                        }
+                        hmap
+                    })
+                    .collect();
+                Ok(inner_result)
             }
-            map.end()
-        }
+            Err(err) => Err(err),
+        };
+
+        let excel_response = ExcelResponse {
+            data,
+            requesters_id: request.requesters_id,
+        };
+
+        res.push(excel_response);
     }
 
-    impl OrderedJson {
-        pub fn new() -> Self {
-            OrderedJson(IndexMap::new())
-        }
-        pub fn insert(&mut self, key: String, value: Value) {
-            self.0.insert(key, value);
-        }
-    }
-
-    let results: Vec<_> = rows
-        .into_iter()
-        .map(|row| {
-            let mut hmap: OrderedJson = OrderedJson::new();
-
-            for (i, column) in row.columns().into_iter().enumerate() {
-                let k = column.name().to_string();
-                let v: serde_json::Value = match *column.type_() {
-                    Type::BOOL => match row.try_get::<_, bool>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::CHAR => match row.try_get::<_, i8>(i) {
-                        Ok(v) => {
-                            let ch = char::from_u32(v as u32).unwrap_or('\0');
-                            serde_json::json!(ch.to_string())
-                        }
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::INT2 => match row.try_get::<_, i16>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::INT4 => match row.try_get::<_, i32>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::OID => match row.try_get::<_, u32>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::INT8 => match row.try_get::<_, i64>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::FLOAT4 => match row.try_get::<_, f32>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::FLOAT8 => match row.try_get::<_, f64>(i) {
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                    Type::DATE => match row.try_get::<_, NaiveDate>(i) {
-                        Ok(v) => {
-                            // let base_date = NaiveDate::from_ymd_opt(1899, 12, 30).unwrap();
-                            // let duration = v.signed_duration_since(base_date);
-                            // serde_json::json!(duration.num_days())
-                            serde_json::json!(v.format("%Y-%m-%d").to_string())
-                        }
-                        Err(_) => serde_json::json!(null),
-                    },
-                    _ => match row.try_get::<_, String>(i) {
-                        //VARCHAR, CHAR(n), TEXT, CITEXT, NAME
-                        Ok(v) => serde_json::json!(v),
-                        Err(_) => serde_json::json!(null),
-                    },
-                };
-                hmap.insert(k, v);
-            }
-            hmap
-        })
-        .collect();
-
-    Ok(serde_json::to_string(&results).map_err(Error::JsonSerializationError)?)
+    res
 }
 
 fn get_db_params() -> HashMap<String, String> {
@@ -203,57 +234,57 @@ fn get_db_params() -> HashMap<String, String> {
     db_parameters
 }
 
-#[derive(Debug)]
+#[derive(Debug)] #[rustfmt::skip]
 enum Error {
-    InvalidUTF16FromVbaError,
-    DBConnectionError(tokio_postgres::Error),
-    SqlExecutionError(tokio_postgres::Error),
-    TokioRuntimeCreationError(std::io::Error),
-    JsonSerializationError(serde_json::Error),
-    JsonDeserializationError(serde_json::Error),
+    InvalidUTF16FromVba,
+    DBConnection(tokio_postgres::Error),
+    SqlExecution(tokio_postgres::Error),
+    TokioRuntimeCreation(std::io::Error),
+    JsonSerialization(serde_json::Error),
+    JsonDeserialization(serde_json::Error),
 }
 
+#[rustfmt::skip]
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Error::DBConnectionError(err) => write!(
-                f,
-                "{}: {}",
-                "01",
-                format!("не удалось подключение к базе данных: {}", err)
+            Error::DBConnection(err) => write!(
+                f,"{}: {}", "01",
+                format!("не удалось подключение к базе данных:\r\n{}", err)
             ),
-            Error::SqlExecutionError(err) => write!(
-                f,
-                "{}: {}",
-                "02",
-                format!("не удалось выполнить SQL-запрос: {}", err)
+            Error::SqlExecution(err) => write!(
+                f,"{}: {}", "02",
+                format!("не удалось выполнить SQL-запрос:\r\n{}", err)
             ),
-            Error::InvalidUTF16FromVbaError => write!(
-                f,
-                "{}: {}",
-                "10", "не удалось конвертировать запрос в UTF-16"
+            Error::JsonDeserialization(err) => write!(
+                f,"{}: {}", "03",
+                format!("не валидные аргументы переданы от vba в dll:\r\n{}", err)
             ),
-            Error::JsonSerializationError(err) => write!(
-                f,
-                "{}: {}",
-                "11",
-                format!("не удалось сериализовать ответ БД в JSON-формат: {}", err)
+            Error::InvalidUTF16FromVba => write!(
+                f,"{}: {}", "10", "не удалось конвертировать запрос в UTF-16"
             ),
-            Error::TokioRuntimeCreationError(err) => write!(
-                f,
-                "{}: {}",
-                "12",
-                format!("не удалось создать рантайм Tokio: {}", err)
+            Error::JsonSerialization(err) => write!(
+                f,"{}: {}", "11",
+                format!("не удалось сериализовать ответ БД в JSON-формат:\r\n{}", err)
             ),
-            Error::JsonDeserializationError(err) => write!(
-                f,
-                "{}: {}",
-                "03",
-                format!("не валидные аргументы переданы от vba в dll: {}", err)
+            Error::TokioRuntimeCreation(err) => write!(
+                f,"{}: {}", "12",
+                format!("не удалось создать рантайм Tokio:\r\n{}", err)
             ),
         }
     }
 }
+
+impl Serialize for Error {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let error_str = self.to_string();
+        serializer.serialize_str(&error_str)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
